8a29553985d2903291a550b7a352c03b9b43618a
Ali Jones <alicj@uw.edu>
2023-01-20 15:42:18 -0800

    ask for user input in fancy_dancing

--- picarx/fancy_dancing.py
+++ picarx/fancy_dancing.py
@@ -3,7 +3,8 @@ from picarx_improved import Picarx
 
 px = Picarx()
 
-def steering_calibration(speed):
+def steering_calibration():
+    speed = int(input("Enter a speed: "))
     px.forward(speed)
     time.sleep(3)
     px.stop()
