4ece1cbb6b040709af82a8c83e74b33ee5f016c5
Ali Jones <jonesal9@oregonstate.edu>
2023-01-21 00:39:12 +0000

    unprinting path

--- picarx/fancy_dancing.py
+++ picarx/fancy_dancing.py
@@ -6,10 +6,10 @@ sys.path.append('/home/ali_pi/robot-hat')
 px = Picarx()
 
 def steering_calibration():
-    speed = int(input("Enter a speed: "))
+    speed = int(input("Enter a speed (??): "))
+    t = int(input("Enter a time (s): "))
     px.forward(speed)
-    time.sleep(3)
+    time.sleep(t)
     px.stop()
 
 steering_calibration()
-print(sys.path)
